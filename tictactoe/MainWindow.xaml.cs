b6856e958c992ba206d1a8dcac6fa66b16d508c4
Kitty Jacobs <kitty.jacobs@immalle.eu>
2017-10-02 10:04:32 +0200

    Lines

--- tictactoe/MainWindow.xaml.cs
+++ tictactoe/MainWindow.xaml.cs
@@ -24,5 +24,32 @@ namespace tictactoe
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Line line1 = new Line();
+            line1.X1 = 0;
+            line1.Y1 = 0;
+            line1.X2 = 300;
+            line1.Y2 = 0;
+            line1.Stroke = new SolidColorBrush(Colors.Black);
+
+            Line line2 = new Line();
+            line2.X1 = 0;
+            line2.Y1 = 0;
+            line2.X2 = 0;
+            line2.Y2 = 300;
+            line2.Stroke = new SolidColorBrush(Colors.Black);
+
+            Line line3 = new Line();
+            line3.X1 = 300;
+            line3.Y1 = 0;
+            line3.X2 = 0;
+            line3.Y2 = 0;
+            line3.Stroke = new SolidColorBrush(Colors.Black);
+
+            myCanvas.Children.Add(line1);
+            myCanvas.Children.Add(line2);
+        }
     }
 }
