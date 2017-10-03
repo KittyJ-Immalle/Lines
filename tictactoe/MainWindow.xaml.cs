e43d184c2d624d52f303350945598602c66cb019
Kitty Jacobs <kitty.jacobs@immalle.eu>
2017-10-03 08:42:06 +0200

    Added all lines

--- tictactoe/MainWindow.xaml.cs
+++ tictactoe/MainWindow.xaml.cs
@@ -44,12 +44,54 @@ namespace tictactoe
             Line line3 = new Line();
             line3.X1 = 300;
             line3.Y1 = 0;
-            line3.X2 = 0;
-            line3.Y2 = 0;
+            line3.X2 = 300;
+            line3.Y2 = 300;
             line3.Stroke = new SolidColorBrush(Colors.Black);
+
+            Line line4 = new Line();
+            line4.X1 = 0;
+            line4.Y1 = 300;
+            line4.X2 = 300;
+            line4.Y2 = 300;
+            line4.Stroke = new SolidColorBrush(Colors.Black);
+
+            Line line5 = new Line();
+            line5.X1 = 100;
+            line5.Y1 = 0;
+            line5.X2 = 100;
+            line5.Y2 = 300;
+            line5.Stroke = new SolidColorBrush(Colors.Black);
+
+            Line line6 = new Line();
+            line6.X1 = 200;
+            line6.Y1 = 0;
+            line6.X2 = 200;
+            line6.Y2 = 300;
+            line6.Stroke = new SolidColorBrush(Colors.Black);
+
+            Line line7 = new Line();
+            line7.X1 = 0;
+            line7.Y1 = 100;
+            line7.X2 = 300;
+            line7.Y2 = 100;
+            line7.Stroke = new SolidColorBrush(Colors.Black);
+
+            Line line8 = new Line();
+            line8.X1 = 0;
+            line8.Y1 = 200;
+            line8.X2 = 300;
+            line8.Y2 = 200;
+            line8.Stroke = new SolidColorBrush(Colors.Black);
 
             myCanvas.Children.Add(line1);
             myCanvas.Children.Add(line2);
+            myCanvas.Children.Add(line3);
+            myCanvas.Children.Add(line4);
+            myCanvas.Children.Add(line5);
+            myCanvas.Children.Add(line6);
+            myCanvas.Children.Add(line7);
+            myCanvas.Children.Add(line8);
+
         }
     }
 }
